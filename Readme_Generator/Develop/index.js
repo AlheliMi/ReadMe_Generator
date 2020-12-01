b893bff58d2031bc027d1c96d0d76b31c88a6ff5
Alheli Miranda <alhealhe72@hotmail.com>
2020-11-30 20:29:23 -0600

    working some parts

--- Readme_Generator/Develop/index.js
+++ Readme_Generator/Develop/index.js
@@ -1,16 +1,102 @@
-// array of questions for user
-const questions = [
+const inquirer = require("inquirer");
+const fs = require('fs');
+const axios = require("axios");
+const generate = require('./utils/generateMarkdown');
 
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project´s Name?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Can you Please provide your project's description, including Fearures if yo have or want?"
+    },
+    {
+        type: "input",
+        name: "badge",
+        message: "Do you have a badges links? Please provide them if that you want"
+    },
+    {
+        type: "input",
+        name: "visuals",
+        message: "Do you have images links? Please provide them if that you want"
+    },
+    {
+        type: "input",
+        name: "installation",
+        message: "Provide the installation instructions"
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Provide the project usage"
+    },
+    {
+        type: "input",
+        name: "contributing",
+        message: "Do your Project have the contributing parties?"
+    },
+    {
+        type: "input",
+        name: "licence",
+        message: "Do you have a licence or your badge link? Please provide "
+    },
+    {
+        type: "input",
+        name: "status",
+        message: "What is your Project Status?"
+    },
+    {
+        type: "input",
+        name: "username",
+        message: "What is your github user name?"
+    },
+    {
+        type: "input",
+        name: "repo",
+        message: "What is your repo link?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is your email?"
+    },   
 ];
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+inquirer
+    .prompt(questions)
+    .then(function(data){
+        const queryUrl = `https://api.github.com/users/${data.username}`;
 
-// function to initialize program
+        axios.get(queryUrl).then(function(res) {
+            
+            const githubInfo = {
+                githubImage: res.data.avatar_url,
+                email: res.data.email,
+                profile: res.data.html_url,
+                name: res.data.name
+            };
+            
+          fs.writeFile("README.md", generate(data, githubInfo), function(err) {
+            if (err) {
+              throw err;
+            };
+    
+            console.log("New README file now created with success!");
+          });
+        });
+
+});
+
 function init() {
 
 }
 
-// function call to initialize program
 init();
+
+// function to write README file
+/* function writeToFile(fileName, data) {
+} */
--- Readme_Generator/Develop/utils/generateMarkdown.js
+++ Readme_Generator/Develop/utils/generateMarkdown.js
@@ -1,7 +1,55 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# **${data.title}**
+***
 
+## Description 
+${data.description}
+## Table of contents
+- [Description](#Description)
+- [Installation](#Installation)
+- [How it looks](#Visuals)
+- [Usage](#Usage)
+- [Licence](#Licence)
+- [Contributors](#Contributors)
+- [Test](#Test)
+- [Repository Link](#Repository)
+- [GitHub Info](#GitHub) 
+
+${data.badge}
+
+## Visuals
+  ![${data.title}](${data.visuals})
+  
+## Installation
+_To install necessary depenvies, you should run the following command:_
+  > ${data.installation}
+
+## Usage
+${data.usage}
+
+## Contributors
+${data.contributing}
+
+## Licence
+${data.licence}
+
+## Status
+ The status of the porject at the moment is:
+${data.status}
+
+## Repository
+- [Project Repo](${data.repo})
+
+## Questios, Coments, Sugestions
+If you have any questios about the repo, you can open an issue or contact directly to me at ${data.email}. Or at gitHub.
+
+---
+## GitHub
+![Image of me](${githubInfo.githubImage})
+- ${githubInfo.name}
+- [GitHub Profile](${githubInfo.profile})
+- <${githubInfo.email}>
 `;
 }
 
